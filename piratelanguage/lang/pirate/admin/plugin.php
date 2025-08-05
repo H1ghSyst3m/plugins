0a982e4a0c5c7f60ad8c9c7afb73ebf6a5ad3c9a
Boy132 <mail@boy132.de>
2025-08-05 11:09:41 +0200

    add pirate translations for plugin page

---
+++ piratelanguage/lang/pirate/admin/plugin.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'nav_title' => 'Add-ons',
+    'model_label' => 'Add-on',
+    'model_label_plural' => 'Add-ons',
+
+    'name' => 'Name o\' the Treasure',
+    'update_available' => 'Thar be an update fer this here add-on!',
+    'author' => 'Scallywag Who Made It',
+    'version' => 'Version o\' the Booty',
+    'category' => 'Type o\' Loot',
+    'status' => 'State o\' the Booty',
+    'settings' => 'Ship Settin\'s',
+    'install' => 'Hoist It Aboard',
+    'update' => 'Refit',
+    'enable' => 'Raise the Sails',
+    'disable' => 'Drop Anchor',
+    'import' => 'Smuggle In',
+    'no_plugins' => 'Nay Add-ons in Sight',
+    'from_file' => 'From the Captain\'s Log',
+    'from_url' => 'From the Sea o\' URLs',
+
+    'status_enum' => [
+        'not_installed' => 'Ain\'t Been Hoisted Yet',
+        'disabled' => 'Anchor Be Dropped',
+        'enabled' => 'Sails Be Raised',
+        'errored' => 'Cursed with Errors',
+        'incompatible' => 'Don\'t Belong on This Ship',
+    ],
+
+    'notifications' => [
+        'installed' => 'Add-on be hoisted aboard!',
+        'updated' => 'Add-on\'s been given a fresh coat o\' paint!',
+        'enabled' => 'Add-on be awake and ready, yarrr!',
+        'disabled' => 'Add-on be sleepin\' in the brig.',
+        'downloaded' => 'Add-on plundered from the net!',
+        'download_failed' => 'Blast it! Couldn\'t snatch the add-on!',
+    ],
+];
